987d1cbf951864742e9dfe6366761862f5d9c952
mrushidany <mrushidany@gmail.com>
2021-12-27 13:03:08 +0300

    Include the vendor scripts into the landing_page blase

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -9,6 +9,8 @@
     <body>
         <div id="landingPage"></div>
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/manifest.js') }}"></script>
+        <script src="{{ asset('js/vendor.js') }}"></script>
+        <script src="{{ asset('js/app.js') }}"></script>
     </body>
 </html>
