f09b538becac6ec00dc64ece678f01812021bf7d
mrushidany <mrushidany@gmail.com>
2021-12-27 12:48:43 +0300

    Scripts declared down

--- resources/views/landing_page.blade.php
+++ resources/views/landing_page.blade.php
@@ -5,10 +5,10 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>ehardwares.co.tz</title>
-
-        <script src="{{ asset('js/app.js') }}" defer></script>
     </head>
     <body>
         <div id="landingPage"></div>
+
+        <script src="{{ asset('js/app.js') }}" defer></script>
     </body>
 </html>
