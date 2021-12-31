78c3c8b47afe70244745d05fbc0f6d825a9a14b4
mrushidany <mrushidany@gmail.com>
2021-12-31 13:10:44 +0300

    Install laravel breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,7 @@
 require('./bootstrap');
 
-require('./components/LandingPage');
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
--- resources/js/components/LandingPage.js
+++ resources/js/components/LandingPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import '../../css/tailwind/ehardware.css'
 
 export default function LandingPage(){
     return (
