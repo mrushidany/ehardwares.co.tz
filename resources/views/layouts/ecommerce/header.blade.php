49e25b8dbd10f76b16d8367a67042a1925a8a252
mrushidany <mrushidany@gmail.com>
2022-01-10 14:07:16 +0300

    Removed the top header

--- resources/views/layouts/ecommerce/header.blade.php
+++ resources/views/layouts/ecommerce/header.blade.php
@@ -1,38 +1,5 @@
 <header>
-    <div id="top-header">
-        <div class="container">
-            <ul class="header-links pull-left">
-                <li>
-                    <a href="#">
-                        <i class="fa fa-phone"></i> +021-95-51-84
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-envelope-o"></i>
-                        <span class="__cf_email__" data-cfemail="442129252d28042129252d286a272b29">[email&#160;protected]</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-map-marker"></i> 1734 Stonecoal Road
-                    </a>
-                </li>
-            </ul>
-            <ul class="header-links pull-right">
-                <li>
-                    <a href="#">
-                        <i class="fa fa-dollar"></i> USD
-                    </a>
-                </li>
-                <li>
-                    <a href="#">
-                        <i class="fa fa-user-o"></i> My Account
-                    </a>
-                </li>
-            </ul>
-        </div>
-    </div>
+
     <div id="header">
         <div class="container">
             <div class="row">
