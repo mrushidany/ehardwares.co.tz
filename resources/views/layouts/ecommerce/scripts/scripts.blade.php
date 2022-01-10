67e350a8c5d0b286518cc58ef28a42595c90f5ce
mrushidany <mrushidany@gmail.com>
2022-01-10 13:03:51 +0300

    Blade templatelization and scripts customization

---
+++ resources/views/layouts/ecommerce/scripts/scripts.blade.php
@@ -0,0 +1,17 @@
+<script src="{{ asset('ecommerce/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/js/bootstrap.min.js%2bslick.min.js.pagespeed.jc.3oMYTFFUTv.js') }}"></script>
+    <script>eval(mod_pagespeed_nePFsnc3nV);</script>
+    <script>eval(mod_pagespeed_wHSoBauwut);</script>
+    <script src="{{ asset('ecommerce/js/nouislider.min.js%2bjquery.zoom.min.js%2bmain.js.pagespeed.jc.UI7AC5_B6N.js') }}"></script>
+    <script>eval(mod_pagespeed_WlmYdmNQnI);</script>
+    <script>eval(mod_pagespeed_HxeDIjvTXK);</script>
+    <script>eval(mod_pagespeed_m624O2n4N4);</script>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-23581568-13"></script>
+    <script>
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'UA-23581568-13');
+    </script>
+    <script defer src="https://static.cloudflareinsights.com/beacon.min.js/v652eace1692a40cfa3763df669d7439c1639079717194" integrity="sha512-Gi7xpJR8tSkrpF7aordPZQlW2DLtzUlZcumS8dMQjwDHEnw9I7ZLyiOj/6tZStRBGtGgN6ceN6cMH8z7etPGlw==" data-cf-beacon='{"rayId":"6c86d3c8f87b88aa","token":"cd0b4b3a733644fc843ef0b185f98241","version":"2021.12.0","si":100}' crossorigin="anonymous"></script>
