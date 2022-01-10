22d5beb6ccc1166159478f09b1e40754112c2c72
mrushidany <mrushidany@gmail.com>
2022-01-10 14:25:40 +0300

    Updated the ehardwares icon

--- resources/views/layouts/ecommerce/stylesheets/link_stylesheets.blade.php
+++ resources/views/layouts/ecommerce/stylesheets/link_stylesheets.blade.php
@@ -1,4 +1,6 @@
+<!-- Favicon -->
+<link href="{{ asset('ecommerce/img/icon.jpeg') }}" rel="icon">
 
+{{-- Stylesheets --}}
 <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
-
 <link type="text/css" rel="stylesheet" href="{{ asset('ecommerce/css/main.css')}}" />
