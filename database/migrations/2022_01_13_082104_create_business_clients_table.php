b57b9c24158a2ab7f83206afbe2d49c0808bf723
mrushidany <mrushidany@gmail.com>
2022-01-13 11:22:52 +0300

    User management database design and migration

---
+++ database/migrations/2022_01_13_082104_create_business_clients_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBusinessClientsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('business_clients', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('role_user_id');
+            $table->foreign('role_user_id')->references('id')->on('role_user')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+            $table->softDeletesTz($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('business_clients');
+    }
+}
