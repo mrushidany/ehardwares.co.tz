8f878bc2a6225681aebe68e5a97b4c4781c5c642
mrushidany <mrushidany@gmail.com>
2022-01-13 11:19:08 +0300

    Database design, users and normal_clients database design

--- database/migrations/2022_01_13_080915_create_normal_clients_table.php
+++ database/migrations/2022_01_13_080915_create_normal_clients_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateUsersTable extends Migration
+class CreateNormalClientsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('normal_clients', function (Blueprint $table) {
             $table->id();
-            $table->string('first_name');
-            $table->string('last_name');
-            $table->string('phone_number');
-            $table->string('phone_number');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->unsignedBigInteger('role_user_id');
+            $table->foreign('role_user_id')->references('id')->on('role_user')->onDelete('cascade')->onUpdate('cascade');
             $table->timestamps();
             $table->softDeletesTz($column = 'deleted_at', $precision = 0);
         });
@@ -35,6 +29,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('normal_clients');
     }
 }
