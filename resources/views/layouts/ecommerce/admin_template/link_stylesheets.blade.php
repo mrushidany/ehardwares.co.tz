dd3eebdf06606104d1dabb988cbc96f12b926fe7
mrushidany <mrushidany@gmail.com>
2022-01-14 17:33:07 +0300

    Integration with the scripts links

--- resources/views/layouts/ecommerce/admin_template/link_stylesheets.blade.php
+++ resources/views/layouts/ecommerce/admin_template/link_stylesheets.blade.php
@@ -24,18 +24,4 @@
     <link href="{{ asset('ecommerce/vendors/overlayscrollbars/OverlayScrollbars.min.css') }}" rel="stylesheet">
     <link href="{{ asset('ecommerce/assets/css/theme.min.css') }}" rel="stylesheet" id="style-default">
     <link href="{{ asset('ecommerce/assets/css/user.min.css') }}" rel="stylesheet" id="user-style-default">
-    <script>
-      var isRTL = JSON.parse(localStorage.getItem('isRTL'));
-      if (isRTL) {
-        var linkDefault = document.getElementById('style-default');
-        var userLinkDefault = document.getElementById('user-style-default');
-        linkDefault.setAttribute('disabled', true);
-        userLinkDefault.setAttribute('disabled', true);
-        document.querySelector('html').setAttribute('dir', 'rtl');
-      } else {
-        var linkRTL = document.getElementById('style-rtl');
-        var userLinkRTL = document.getElementById('user-style-rtl');
-        linkRTL.setAttribute('disabled', true);
-        userLinkRTL.setAttribute('disabled', true);
-      }
-    </script>
+
--- resources/views/layouts/ecommerce/admin_template/scripts_link.blade.php
+++ resources/views/layouts/ecommerce/admin_template/scripts_link.blade.php
@@ -1,13 +1,13 @@
 <!-- ===============================================-->
     <!--    JavaScripts-->
     <!-- ===============================================-->
-    <script src="vendors/popper/popper.min.js"></script>
-    <script src="vendors/bootstrap/bootstrap.min.js"></script>
-    <script src="vendors/anchorjs/anchor.min.js"></script>
-    <script src="vendors/is/is.min.js"></script>
-    <script src="vendors/echarts/echarts.min.js"></script>
-    <script src="vendors/fontawesome/all.min.js"></script>
-    <script src="vendors/lodash/lodash.min.js"></script>
-    <script src="../../../polyfill.io/v3/polyfill.min58be.js?features=window.scroll"></script>
-    <script src="vendors/list.js/list.min.js"></script>
-    <script src="assets/js/theme.js"></script>
+    <script src="{{ asset('ecommerce/vendors/popper/popper.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/bootstrap/bootstrap.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/anchorjs/anchor.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/is/is.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/echarts/echarts.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/fontawesome/all.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/lodash/lodash.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/polyfill/polyfill.min58be.js?features=window.scroll') }}"></script>
+    <script src="{{ asset('ecommerce/vendors/list.js/list.min.js') }}"></script>
+    <script src="{{ asset('ecommerce/assets/js/theme.js') }}"></script>
