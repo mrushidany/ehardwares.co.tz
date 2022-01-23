69f830511dc3656479c00f6e0ef89b99c4930520
mrushidany <mrushidany@gmail.com>
2022-01-23 11:35:58 +0300

    Added the new user button

--- resources/views/ecommerce/admin/super/settings/all_users.blade.php
+++ resources/views/ecommerce/admin/super/settings/all_users.blade.php
@@ -16,6 +16,15 @@
             <div class="card mb-3">
                 <div class="card-body position-relative">
                     <div class="row">
+                        <div class="col-auto ms-auto">
+                            <div class="nav nav-pills nav-pills-falcon flex-grow-1">
+                                <span>
+                                    <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button"><i class="fas fa-plus"></i>&nbsp;User</button>
+                                </span>
+                            </div>
+                        </div>
+                    </div><br>
+                    <div class="row">
                         <div class="col-lg-12">
                             <div class="card-body pt-0">
                                 <table class="table table-striped table-bordered all_users_table" style="width:100%">
