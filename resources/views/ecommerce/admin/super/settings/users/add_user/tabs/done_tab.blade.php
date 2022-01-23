8034e5ad83c93aa352f67b2100c7fa9e8c230850
mrushidany <mrushidany@gmail.com>
2022-01-23 13:19:03 +0300

    Done tab

---
+++ resources/views/ecommerce/admin/super/settings/users/add_user/tabs/done_tab.blade.php
@@ -0,0 +1,8 @@
+<div class="tab-pane text-center px-sm-3 px-md-5" role="tabpanel" aria-labelledby="bootstrap-wizard-validation-tab4" id="bootstrap-wizard-validation-tab4">
+    <div class="wizard-lottie-wrapper">
+        <div class="lottie wizard-lottie mx-auto my-3" data-options="{&quot;path&quot;:&quot;../../assets/img/animated-icons/celebration.json&quot;}"></div>
+    </div>
+    <h4 class="mb-1">New user is all set!</h4>
+    <p>Now you can access to your account</p>
+    <a class="btn btn-primary px-5 my-3" href="{{ route('all_users') }}">Start Over</a>
+</div>
