e87c1e13cff5d0e57bc3de8961fb6f62739e4bdd
mrushidany <mrushidany@gmail.com>
2022-01-25 12:15:52 +0300

    Adding the greeting feature

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 
 class DashboardController extends Controller
@@ -14,19 +15,33 @@ class DashboardController extends Controller
 
      public function index()
      {
+        $data = [
+            'greeting' => $this->greeting(),
+        ];
+
          if(Auth::user()->hasRole('super_administrator'))
          {
-            return view('ecommerce.admin.super.index');
+            return view('ecommerce.admin.super.index')->with($data);
          }
          else if(Auth::user()->hasRole('administrator'))
          {
-            return view('ecommerce.admin.junior.index');
+            return view('ecommerce.admin.junior.index')->with($data);
          }
          else if(Auth::user()->hasRole('owner'))
          {
-            return view('ecommerce.admin.owner.index');
+            return view('ecommerce.admin.owner.index')->with($data);
          }
+     }
 
-
+     public function greeting()
+     {
+        $hour = Carbon::now()->format('H');
+        if($hour < 12) {
+            return 'Good Morning';
+        } elseif ($hour < 17) {
+            return 'Good Afternoon';
+        }else {
+            return 'Good Evening';
+        }
      }
 }
