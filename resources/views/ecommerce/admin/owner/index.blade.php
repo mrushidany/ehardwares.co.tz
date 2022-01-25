33fe64a1fef14e1b900c3ef2d1658e4ea09dbe97
mrushidany <mrushidany@gmail.com>
2022-01-25 12:50:20 +0300

    Weekl sales tootltip

--- resources/views/ecommerce/admin/owner/index.blade.php
+++ resources/views/ecommerce/admin/owner/index.blade.php
@@ -8,71 +8,95 @@
                 <div class="card bg-transparent-50 overflow-hidden">
                     <div class="card-header position-relative">
                         <div class="bg-holder d-none d-md-block bg-card z-index-1" style="background-image:url({{ asset('ecommerce/assets/img/illustrations/ecommerce-bg.png') }});background-size:230px;background-position:right bottom;z-index:-1;"></div>
-              <!--/.bg-holder-->
-              <div class="position-relative z-index-2">
-                <div>
-                  <h3 class="text-primary mb-1">{{ $greeting }}, {{ Auth::user()->first_name }} !</h3>
-                  <p>Here’s what happening with <i style="color: orangered"><strong>ehardwares.co.tz<strong></i>&nbsp; today </p>
+                        <!--/.bg-holder-->
+                        <div class="position-relative z-index-2">
+                            <div>
+                                <h3 class="text-primary mb-1">{{ $greeting }}, {{ Auth::user()->first_name }} !</h3>
+                                    <p>Here’s what happening with
+                                        <i style="color: orangered">
+                                            <strong>ehardwares.co.tz</strong>
+                                        </i>&nbsp;today
+                                    </p>
+                            </div>
+                            <div class="d-flex py-3">
+                                <div class="pe-3">
+                                    <p class="text-600 fs--1 fw-medium">Today's visit </p>
+                                    <h4 class="text-800 mb-0">3</h4>
+                                </div>
+                                <div class="ps-3">
+                                    <p class="text-600 fs--1">Today’s total sales </p>
+                                    <h4 class="text-800 mb-0">10,000 /=</h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-body p-0">
+                        <ul class="mb-0 list-unstyled">
+                            <li class="alert mb-0 rounded-0 py-3 px-card alert-warning border-x-0 border-top-0">
+                                <div class="row flex-between-center">
+                                    <div class="col">
+                                        <div class="d-flex">
+                                            <div class="fas fa-circle mt-1 fs--2"></div>
+                                            <p class="fs--1 ps-2 mb-0">
+                                                <strong>5 invoices</strong> needs to be approved
+                                            </p>
+                                        </div>
+                                    </div>
+                                     <div class="col-auto d-flex align-items-center">
+                                        <a class="alert-link fs--1 fw-medium" href="#!">View products
+                                            <i class="fas fa-chevron-right ms-1 fs--2"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="alert mb-0 rounded-0 py-3 px-card greetings-item border-top border-x-0 border-top-0">
+                                <div class="row flex-between-center">
+                                    <div class="col">
+                                        <div class="d-flex">
+                                            <div class="fas fa-circle mt-1 fs--2 text-primary"></div>
+                                            <p class="fs--1 ps-2 mb-0">
+                                                <strong>7 orders</strong> have payments that need to be captured
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="col-auto d-flex align-items-center">
+                                        <a class="alert-link fs--1 fw-medium" href="#!">View payments
+                                            <i class="fas fa-chevron-right ms-1 fs--2"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                            <li class="alert mb-0 rounded-0 py-3 px-card greetings-item border-top  border-0">
+                                <div class="row flex-between-center">
+                                    <div class="col">
+                                        <div class="d-flex">
+                                            <div class="fas fa-circle mt-1 fs--2 text-primary"></div>
+                                            <p class="fs--1 ps-2 mb-0">
+                                                <strong>50+ orders</strong> need to be fulfilled
+                                            </p>
+                                        </div>
+                                    </div>
+                                    <div class="col-auto d-flex align-items-center">
+                                        <a class="alert-link fs--1 fw-medium" href="#!">View orders
+                                            <i class="fas fa-chevron-right ms-1 fs--2"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-                <div class="d-flex py-3">
-                  <div class="pe-3">
-                    <p class="text-600 fs--1 fw-medium">Today's visit </p>
-                    <h4 class="text-800 mb-0"></h4>
-                  </div>
-                  <div class="ps-3">
-                    <p class="text-600 fs--1">Today’s total sales </p>
-                    <h4 class="text-800 mb-0"> /=</h4>
-                  </div>
-                </div>
-              </div>
             </div>
-            <div class="card-body p-0">
-              <ul class="mb-0 list-unstyled">
-                <li class="alert mb-0 rounded-0 py-3 px-card alert-warning border-x-0 border-top-0">
-                  <div class="row flex-between-center">
-                    <div class="col">
-                      <div class="d-flex">
-                        <svg class="svg-inline--fa fa-circle fa-w-16 mt-1 fs--2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path></svg><!-- <div class="fas fa-circle mt-1 fs--2"></div> Font Awesome fontawesome.com -->
-                        <p class="fs--1 ps-2 mb-0"><strong>5 invoices</strong> needs to be approved</p>
-                      </div>
-                    </div>
-                    <div class="col-auto d-flex align-items-center"><a class="alert-link fs--1 fw-medium" href="#!">View products<svg class="svg-inline--fa fa-chevron-right fa-w-10 ms-1 fs--2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><!-- <i class="fas fa-chevron-right ms-1 fs--2"></i> Font Awesome fontawesome.com --></a></div>
-                  </div>
-                </li>
-                <li class="alert mb-0 rounded-0 py-3 px-card greetings-item border-top border-x-0 border-top-0">
-                  <div class="row flex-between-center">
-                    <div class="col">
-                      <div class="d-flex">
-                        <svg class="svg-inline--fa fa-circle fa-w-16 mt-1 fs--2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path></svg><!-- <div class="fas fa-circle mt-1 fs--2 text-primary"></div> Font Awesome fontawesome.com -->
-                        <p class="fs--1 ps-2 mb-0"><strong>7 orders</strong> have payments that need to be captured</p>
-                      </div>
-                    </div>
-                    <div class="col-auto d-flex align-items-center"><a class="alert-link fs--1 fw-medium" href="#!">View payments<svg class="svg-inline--fa fa-chevron-right fa-w-10 ms-1 fs--2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><!-- <i class="fas fa-chevron-right ms-1 fs--2"></i> Font Awesome fontawesome.com --></a></div>
-                  </div>
-                </li>
-                <li class="alert mb-0 rounded-0 py-3 px-card greetings-item border-top  border-0">
-                  <div class="row flex-between-center">
-                    <div class="col">
-                      <div class="d-flex">
-                        <svg class="svg-inline--fa fa-circle fa-w-16 mt-1 fs--2 text-primary" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="circle" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" data-fa-i2svg=""><path fill="currentColor" d="M256 8C119 8 8 119 8 256s111 248 248 248 248-111 248-248S393 8 256 8z"></path></svg><!-- <div class="fas fa-circle mt-1 fs--2 text-primary"></div> Font Awesome fontawesome.com -->
-                        <p class="fs--1 ps-2 mb-0"><strong>50+ orders</strong> need to be fulfilled</p>
-                      </div>
-                    </div>
-                    <div class="col-auto d-flex align-items-center"><a class="alert-link fs--1 fw-medium" href="#!">View orders<svg class="svg-inline--fa fa-chevron-right fa-w-10 ms-1 fs--2" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="chevron-right" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512" data-fa-i2svg=""><path fill="currentColor" d="M285.476 272.971L91.132 467.314c-9.373 9.373-24.569 9.373-33.941 0l-22.667-22.667c-9.357-9.357-9.375-24.522-.04-33.901L188.505 256 34.484 101.255c-9.335-9.379-9.317-24.544.04-33.901l22.667-22.667c9.373-9.373 24.569-9.373 33.941 0L285.475 239.03c9.373 9.372 9.373 24.568.001 33.941z"></path></svg><!-- <i class="fas fa-chevron-right ms-1 fs--2"></i> Font Awesome fontawesome.com --></a></div>
-                  </div>
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-        <div class="col-lg-12">
-          <div class="row g-3">
-            <div class="col-md-6">
-              <div class="card h-md-100 ecommerce-card-min-width">
-                <div class="card-header pb-0">
-                  <h6 class="mb-0 mt-2 d-flex align-items-center">Weekly Sales
-                      <span class="ms-1 text-400" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Calculated according to last week's sales" aria-label="Calculated according to last week's sales">
-                          <!-- <span class="far fa-question-circle" data-fa-transform="shrink-1"></span> Font Awesome fontawesome.com --></span></h6>
+            <div class="col-lg-12">
+                <div class="row g-3">
+                    <div class="col-md-6">
+                        <div class="card h-md-100 ecommerce-card-min-width">
+                            <div class="card-header pb-0">
+                                <h6 class="mb-0 mt-2 d-flex align-items-center">Weekly Sales
+                                    <span class="ms-1 text-400" data-bs-toggle="tooltip" data-bs-placement="top" title="" data-bs-original-title="Calculated according to last week's sales" aria-label="Calculated according to last week's sales">
+                                        <span class="far fa-question-circle" data-fa-transform="shrink-1"></span>
+                                    </span>
+                                </h6>
                 </div>
                 <div class="card-body d-flex flex-column justify-content-end">
                   <div class="row">
