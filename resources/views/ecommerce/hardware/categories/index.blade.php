a513734bacbd6bf862c8f0a6c339ee4079b7f82d
mrushidany <mrushidany@gmail.com>
2022-02-04 15:48:36 +0300

    Created a workspace for hardware categories

--- resources/views/ecommerce/hardware/categories/index.blade.php
+++ resources/views/ecommerce/hardware/categories/index.blade.php
@@ -15,6 +15,39 @@
         </div>
     </div>
     <div class="card-body bg-light">
+        <div class="card mb-3">
+            <div class="card-body position-relative">
+                <div class="row">
+                    <div class="col-auto ms-auto">
+                        <div class="nav nav-pills nav-pills-falcon flex-grow-1">
+                            <span>
+                                <a class="btn btn-outline-primary btn-sm me-1 mb-1" href="{{ route('admin_add_user') }}"><i class="fas fa-plus"></i>&nbsp;User</a>
+                            </span>
+                        </div>
+                    </div>
+                </div><br>
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="card-body pt-0">
+                            <table class="table table-striped table-bordered all_users_table" style="width:100%">
+                                <thead>
+                                    <tr>
+                                        <th>Name</th>
+                                        <th>Email</th>
+                                        <th>Phone Number</th>
+                                        <th>Role</th>
+                                        <th></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
 
     </div>
 </div>
