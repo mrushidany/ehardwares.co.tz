f804d7ad3002fd8e67b195779b4dbd6c13ca6b12
mrushidany <mrushidany@gmail.com>
2022-02-04 16:24:17 +0300

    Removed the link button and replace it with a modal

--- resources/views/ecommerce/hardware/categories/index.blade.php
+++ resources/views/ecommerce/hardware/categories/index.blade.php
@@ -21,7 +21,39 @@
                     <div class="col-auto ms-auto">
                         <div class="nav nav-pills nav-pills-falcon flex-grow-1">
                             <span>
-                                <a class="btn btn-outline-primary btn-sm me-1 mb-1 category_button" href=""><i class="fas fa-plus"></i>&nbsp;Category</a>
+                                <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button" data-bs-toggle="modal" data-bs-target="#error-modal">
+                                    <i class="fas fa-plus"></i>&nbsp;Category
+                                </button>
+<div class="modal fade" id="error-modal" tabindex="-1" role="dialog" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 500px">
+    <div class="modal-content position-relative">
+      <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
+        <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body p-0">
+        <div class="rounded-top-lg py-3 ps-4 pe-6 bg-light">
+          <h4 class="mb-1" id="modalExampleDemoLabel">Add a new illustration </h4>
+        </div>
+        <div class="p-4 pb-0">
+          <form>
+            <div class="mb-3">
+              <label class="col-form-label" for="recipient-name">Recipient:</label>
+              <input class="form-control" id="recipient-name" type="text" />
+            </div>
+            <div class="mb-3">
+              <label class="col-form-label" for="message-text">Message:</label>
+              <textarea class="form-control" id="message-text"></textarea>
+            </div>
+          </form>
+        </div>
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
+        <button class="btn btn-primary" type="button">Understood </button>
+      </div>
+    </div>
+  </div>
+</div>
                             </span>
                         </div>
                     </div>
