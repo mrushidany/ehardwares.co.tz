7aec75122184a417a372620036c9ce2e9dbe6344
mrushidany <mrushidany@gmail.com>
2022-02-04 16:55:33 +0300

    Customization of the hardware category form

--- resources/views/ecommerce/hardware/categories/hardware_category_form.blade.php
+++ resources/views/ecommerce/hardware/categories/hardware_category_form.blade.php
@@ -1,28 +1,28 @@
-<div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 500px">
+<div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 700px">
     <div class="modal-content position-relative">
-        <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
-            <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
+        <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body p-0">
+        <div class="rounded-top-lg py-3 ps-4 pe-6 bg-light">
+          <h4 class="mb-1" id="modalExampleDemoLabel">Add a new hardware category </h4>
         </div>
-        <div class="modal-body p-0">
-            <div class="rounded-top-lg py-3 ps-4 pe-6 bg-light">
-                <h4 class="mb-1" id="modalExampleDemoLabel">Add a new illustration </h4>
+        <div class="p-4 pb-0">
+          <form>
+            <div class="mb-3">
+              <label class="col-form-label" for="recipient-name">Recipient:</label>
+              <input class="form-control" id="recipient-name" type="text">
             </div>
-            <div class="p-4 pb-0">
-                <form>
-                    <div class="mb-3">
-                        <label class="col-form-label" for="recipient-name">Recipient:</label>
-                        <input class="form-control" id="recipient-name" type="text" />
-                    </div>
-                    <div class="mb-3">
-                        <label class="col-form-label" for="message-text">Message:</label>
-                        <textarea class="form-control" id="message-text"></textarea>
-                    </div>
-                </form>
+            <div class="mb-3">
+              <label class="col-form-label" for="message-text">Message:</label>
+              <textarea class="form-control" id="message-text"></textarea>
             </div>
+          </form>
         </div>
-        <div class="modal-footer">
-            <button class="btn btn-secondary" type="button" data-bs-dismiss="modal">Close</button>
-            <button class="btn btn-primary" type="button">Understood </button>
-        </div>
+      </div>
+      <div class="modal-footer">
+        <button class="btn btn-outline-warning me-1 mb-1" type="reset">Reset</button>
+        <button class="btn btn-outline-success me-1 mb-1" type="button">Save</button>
+      </div>
     </div>
-</div>
+  </div>
