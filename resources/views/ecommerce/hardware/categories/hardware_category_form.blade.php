5047d1d1f066343f67eb93924402e91630611ccd
mrushidany <mrushidany@gmail.com>
2022-02-04 17:05:21 +0300

    Reallignment

--- resources/views/ecommerce/hardware/categories/hardware_category_form.blade.php
+++ resources/views/ecommerce/hardware/categories/hardware_category_form.blade.php
@@ -1,34 +1,30 @@
 <div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 700px">
     <div class="modal-content position-relative">
-      <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
-        <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body p-0">
-        <div class="rounded-top-lg py-3 ps-4 pe-6 bg-light">
-          <h4 class="mb-1" id="modalExampleDemoLabel">Add a new hardware category </h4>
+        <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
+            <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        <div class="p-4 pb-0">
-          <form>
-            <div class="row">
-                <div class="mb-3 col-6">
-                    <label class="col-form-label" for="hardware_category_name">Name</label>
-                    <input class="form-control" name="category_name" type="text">
-                  </div>
-
-                  <div class="mb-3 col-6">
-                    <label class="col-form-label" for="message-text">Description</label>
-                    <textarea class="form-control" rows="1" name="description"></textarea>
-                  </div>
-
-              </div>
-
-
-          </form>
-        </div>
-      </div>
-      <div class="modal-footer">
-        <button class="btn btn-outline-warning me-1 mb-1" type="reset">Reset</button>
-        <button class="btn btn-outline-success me-1 mb-1" type="button">Save</button>
-      </div>
+        <form>
+            <div class="modal-body p-0">
+                <div class="rounded-top-lg py-3 ps-4 pe-6 bg-light">
+                    <h4 class="mb-1" id="modalExampleDemoLabel">Add a new hardware category </h4>
+                </div>
+                <div class="p-4 pb-0">
+                    <div class="row">
+                        <div class="mb-3 col-6">
+                            <label class="col-form-label" for="hardware_category_name">Name</label>
+                            <input class="form-control" name="category_name" type="text">
+                        </div>
+                        <div class="mb-3 col-6">
+                            <label class="col-form-label" for="message-text">Description</label>
+                            <textarea class="form-control" rows="1" name="description"></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-outline-warning me-1 mb-1" type="reset">Reset</button>
+                <button class="btn btn-outline-success me-1 mb-1" type="button">Save</button>
+            </div>
+        </form>
     </div>
-  </div>
+</div>
