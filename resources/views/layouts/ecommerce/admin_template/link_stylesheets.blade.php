27ece74c5b44bd76308895829c71c8e1dd618e02
mrushidany <mrushidany@gmail.com>
2022-02-07 15:20:37 +0300

    iziToast inclusion in the header section

--- resources/views/layouts/ecommerce/admin_template/link_stylesheets.blade.php
+++ resources/views/layouts/ecommerce/admin_template/link_stylesheets.blade.php
@@ -30,3 +30,8 @@
     <!-- ===============================================-->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css" />
 
+     <!-- ===============================================-->
+    <!--    iziToast-->
+    <!-- ===============================================-->
+    <link rel="stylesheet" href="{{ asset('ecommerce/iziToast/dist/css/iziToast.min.css') }}" />
+
