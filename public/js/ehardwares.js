8a93f30a68342b68ad518cc1ef0074da3e181c7e
mrushidany <mrushidany@gmail.com>
2022-02-07 14:07:54 +0300

    Changed the route name from the ajax url

--- public/js/ehardwares.js
+++ public/js/ehardwares.js
@@ -31,7 +31,7 @@ $('.save_hardware_category').on('click', function() {
         $('.loading_button').removeAttr('style');
 
         $.ajax({
-            url: "{{ route('hardware_categories.store') }}",
+            url: "{{ route('save_hardware_category') }}",
             method: "POST",
             data:
             {
