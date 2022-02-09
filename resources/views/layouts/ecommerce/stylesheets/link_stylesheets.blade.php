a4f9238df5714125c0e4e32d7a208ca9eee613bf
mrushidany <mrushidany@gmail.com>
2022-02-09 15:04:54 +0300

    Removed the previous design landing_page

--- resources/views/layouts/ecommerce/stylesheets/link_stylesheets.blade.php
+++
@@ -1,6 +0,0 @@
-<!-- Favicon -->
-<link href="{{ asset('ecommerce/img/icon.jpeg') }}" rel="icon">
-
-{{-- Stylesheets --}}
-<link href="https://fonts.googleapis.com/css?family=Montserrat:400,500,700" rel="stylesheet">
-<link type="text/css" rel="stylesheet" href="{{ asset('ecommerce/css/main.css')}}" />
