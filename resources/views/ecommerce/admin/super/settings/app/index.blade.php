cda44b36118da7df1cfd7418a4e6846c3542ea6a
mrushidany <mrushidany@gmail.com>
2022-02-14 14:02:16 +0300

    Only one accordion link for now

--- resources/views/ecommerce/admin/super/settings/app/index.blade.php
+++ resources/views/ecommerce/admin/super/settings/app/index.blade.php
@@ -18,7 +18,7 @@
         </div>
         <div class="card-body bg-light">
             <div class="tab-content">
-                <div class="tab-pane preview-tab-pane active" role="tabpanel" aria-labelledby="tab-dom-e8c9367f-b535-4cba-a4c4-53eeebf4822e">
+                <div class="tab-pane preview-tab-pane active" role="tabpanel">
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="heading1">
@@ -28,20 +28,10 @@
                                 <div class="accordion-body">Once you’re set up, payouts arrive in your bank account on a 2-day rolling basis. Or you can opt to receive payouts weekly or monthly.</div>
                             </div>
                         </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="heading2">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapse2" aria-expanded="false" aria-controls="collapse2">How do refunds work?</button>
-                             </h2>
-                            <div class="accordion-collapse collapse" id="collapse2" aria-labelledby="heading2" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">You can issue either partial or full refunds. There are no fees to refund a charge, but the fees from the original charge are not returned.</div>
-                            </div>
-                        </div>
                     </div>
                 </div>
             </div>
         </div>
     </div>
 </div>
-
-
 @endsection
