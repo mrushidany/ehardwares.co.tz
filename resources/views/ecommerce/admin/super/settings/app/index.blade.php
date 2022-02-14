af073d157bca1cfce2d186cd3fb5013da38b0507
mrushidany <mrushidany@gmail.com>
2022-02-14 14:05:10 +0300

    Edit the targets for the database settings

--- resources/views/ecommerce/admin/super/settings/app/index.blade.php
+++ resources/views/ecommerce/admin/super/settings/app/index.blade.php
@@ -22,9 +22,9 @@
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="heading1">
-                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapse1" aria-expanded="true" aria-controls="collapse1">How long do payouts take?</button>
+                                <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#database_settings" aria-expanded="true" aria-controls="collapse1">How long do payouts take?</button>
                             </h2>
-                            <div class="accordion-collapse collapse show" id="collapse1" aria-labelledby="heading1" data-bs-parent="#accordionExample" style="">
+                            <div class="accordion-collapse collapse show" id="database_settings" aria-labelledby="heading1" data-bs-parent="#accordionExample" style="">
                                 <div class="accordion-body">Once you’re set up, payouts arrive in your bank account on a 2-day rolling basis. Or you can opt to receive payouts weekly or monthly.</div>
                             </div>
                         </div>
