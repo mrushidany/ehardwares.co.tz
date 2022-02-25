0c408be5a4ae5e9eced5f2d8db003e06a13352ec
mrushidany <mrushidany@gmail.com>
2022-02-25 14:48:59 +0300

    Created a forms directory for handling the categories forms

--- resources/views/ecommerce/hardware/categories/tabs/main_categories.blade.php
+++ resources/views/ecommerce/hardware/categories/tabs/main_categories.blade.php
@@ -11,7 +11,7 @@
                             <i class="fas fa-file-csv"></i>
                         </button>
                         <div class="modal fade" id="hardware_category_modal" tabindex="-1" role="dialog" aria-hidden="true">
-                            @include('ecommerce.hardware.categories.hardware_category_form')
+                            @include('ecommerce.hardware.categories.forms.hardware_category_form')
                         </div>
                     </span>
                 </div>
