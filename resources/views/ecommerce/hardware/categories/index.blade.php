b9e72fc0a48a33cef564f4da80d187445579aee5
mrushidany <mrushidany@gmail.com>
2022-02-25 12:03:57 +0300

    Tabs files inclusion

--- resources/views/ecommerce/hardware/categories/index.blade.php
+++ resources/views/ecommerce/hardware/categories/index.blade.php
@@ -28,53 +28,15 @@
                     </ul>
                     <div class="tab-content border-x border-bottom p-3" id="myTabContent">
                         <div class="tab-pane fade active show" id="main_categories_tab" role="tabpanel" aria-labelledby="main_categories_tab">
-                            <div class="card mb-3">
-                                <div class="card-body position-relative">
-                                    <div class="row">
-                                        <div class="col-auto ms-auto">
-                                            <div class="nav nav-pills nav-pills-falcon flex-grow-1">
-                                                <span>
-                                                    <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button" data-bs-toggle="modal" data-bs-target="#hardware_category_modal">
-                                                        <i class="fas fa-plus"></i>&nbsp;Category
-                                                    </button>
-                                                    <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button" data-bs-toggle="modal" data-bs-target="#hardware_category_modal">
-                                                        <i class="fas fa-file-csv"></i>
-                                                    </button>
-                                                    <div class="modal fade" id="hardware_category_modal" tabindex="-1" role="dialog" aria-hidden="true">
-                                                        @include('ecommerce.hardware.categories.hardware_category_form')
-                                                    </div>
-                                                </span>
-                                            </div>
-                                        </div>
-                                    </div><br>
-                                    <div class="row">
-                                        <div class="col-lg-12">
-                                            <div class="card-body pt-0">
-                                                <table class="table table-striped table-bordered hardware_categories_table" style="width:100%">
-                                                    <thead>
-                                                        <tr>
-                                                            <th>Category Name</th>
-                                                            <th>Description</th>
-                                                            <th></th>
-                                                        </tr>
-                                                    </thead>
-                                                    <tbody>
-
-                                                    </tbody>
-                                                </table>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
+                            @include('ecommerce.hardware.categories.tabs.main_categories')
                         </div>
-                    <div class="tab-pane fade" id="sub_categories_tab" role="tabpanel" aria-labelledby="sub_categories_tab">
-
+                        <div class="tab-pane fade" id="sub_categories_tab" role="tabpanel" aria-labelledby="sub_categories_tab">
+                            @include('ecommerce.hardware.categories.tabs.sub_categories')
+                        </div>
                     </div>
-                  </div>
                 </div>
 
-              </div>
+            </div>
 
         </div>
     </div>
---
+++ resources/views/ecommerce/hardware/categories/tabs/main_categories.blade.php
@@ -0,0 +1,39 @@
+<div class="card mb-3">
+    <div class="card-body position-relative">
+        <div class="row">
+            <div class="col-auto ms-auto">
+                <div class="nav nav-pills nav-pills-falcon flex-grow-1">
+                    <span>
+                        <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button" data-bs-toggle="modal" data-bs-target="#hardware_category_modal">
+                            <i class="fas fa-plus"></i>&nbsp;Category
+                        </button>
+                        <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button" data-bs-toggle="modal" data-bs-target="#hardware_category_modal">
+                            <i class="fas fa-file-csv"></i>
+                        </button>
+                        <div class="modal fade" id="hardware_category_modal" tabindex="-1" role="dialog" aria-hidden="true">
+                            @include('ecommerce.hardware.categories.hardware_category_form')
+                        </div>
+                    </span>
+                </div>
+            </div>
+        </div><br>
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="card-body pt-0">
+                    <table class="table table-striped table-bordered hardware_categories_table" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>Category Name</th>
+                                <th>Description</th>
+                                <th></th>
+                            </tr>
+                        </thead>
+                        <tbody>
+
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
