00d8e2b4beca713b9f5cb0d0c41b82c4c7186e2e
mrushidany <mrushidany@gmail.com>
2022-02-25 11:55:36 +0300

    Put Hardware categories CRUD into the main categories

--- resources/views/ecommerce/hardware/categories/index.blade.php
+++ resources/views/ecommerce/hardware/categories/index.blade.php
@@ -23,58 +23,59 @@
                             <a class="nav-link active" id="main_categories_tab" data-bs-toggle="tab" href="#main_categories_tab" role="tab" aria-controls="main_categories_tab" aria-selected="true">Main Categories</a>
                         </li>
                         <li class="nav-item">
-                            <a class="nav-link" id="sub_categories_tab" data-bs-toggle="tab" href="#sub_categories_tab" role="tab" aria-controls="tab-profile" aria-selected="false">Sub Cateories</a>
+                            <a class="nav-link" id="sub_categories_tab" data-bs-toggle="tab" href="#sub_categories_tab" role="tab" aria-controls="tab-profile" aria-selected="false">Sub Categories</a>
                         </li>
                     </ul>
                     <div class="tab-content border-x border-bottom p-3" id="myTabContent">
                         <div class="tab-pane fade active show" id="main_categories_tab" role="tabpanel" aria-labelledby="main_categories_tab">
+                            <div class="card mb-3">
+                                <div class="card-body position-relative">
+                                    <div class="row">
+                                        <div class="col-auto ms-auto">
+                                            <div class="nav nav-pills nav-pills-falcon flex-grow-1">
+                                                <span>
+                                                    <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button" data-bs-toggle="modal" data-bs-target="#hardware_category_modal">
+                                                        <i class="fas fa-plus"></i>&nbsp;Category
+                                                    </button>
+                                                    <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button" data-bs-toggle="modal" data-bs-target="#hardware_category_modal">
+                                                        <i class="fas fa-file-csv"></i>
+                                                    </button>
+                                                    <div class="modal fade" id="hardware_category_modal" tabindex="-1" role="dialog" aria-hidden="true">
+                                                        @include('ecommerce.hardware.categories.hardware_category_form')
+                                                    </div>
+                                                </span>
+                                            </div>
+                                        </div>
+                                    </div><br>
+                                    <div class="row">
+                                        <div class="col-lg-12">
+                                            <div class="card-body pt-0">
+                                                <table class="table table-striped table-bordered hardware_categories_table" style="width:100%">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>Category Name</th>
+                                                            <th>Description</th>
+                                                            <th></th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
 
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     <div class="tab-pane fade" id="sub_categories_tab" role="tabpanel" aria-labelledby="sub_categories_tab">
 
                     </div>
+                  </div>
                 </div>
-            </div>
-        </div>
-            <div class="card mb-3">
-                <div class="card-body position-relative">
-                    <div class="row">
-                        <div class="col-auto ms-auto">
-                            <div class="nav nav-pills nav-pills-falcon flex-grow-1">
-                                <span>
-                                    <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button" data-bs-toggle="modal" data-bs-target="#hardware_category_modal">
-                                        <i class="fas fa-plus"></i>&nbsp;Category
-                                    </button>
-                                    <button class="btn btn-outline-primary btn-sm me-1 mb-1" type="button" data-bs-toggle="modal" data-bs-target="#hardware_category_modal">
-                                        <i class="fas fa-file-csv"></i>
-                                    </button>
-                                    <div class="modal fade" id="hardware_category_modal" tabindex="-1" role="dialog" aria-hidden="true">
-                                        @include('ecommerce.hardware.categories.hardware_category_form')
-                                    </div>
-                                </span>
-                            </div>
-                        </div>
-                    </div><br>
-                    <div class="row">
-                        <div class="col-lg-12">
-                            <div class="card-body pt-0">
-                                <table class="table table-striped table-bordered hardware_categories_table" style="width:100%">
-                                    <thead>
-                                        <tr>
-                                            <th>Category Name</th>
-                                            <th>Description</th>
-                                            <th></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
 
-                                    </tbody>
-                                </table>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+              </div>
+
         </div>
     </div>
 </div>
