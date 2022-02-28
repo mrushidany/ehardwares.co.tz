d8decbad7e1294c048de36ea5c1492152ee401fa
mrushidany <mrushidany@gmail.com>
2022-02-28 13:36:45 +0300

    Added the soft deletes option in the Models

--- app/Models/HardwareSubCategory.php
+++ app/Models/HardwareSubCategory.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class HardwareSubCategory extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
 
     public function hardware_main_category()
     {
