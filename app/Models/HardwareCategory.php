d510cd6e7542281f0d483d7ac7f1063849fef214
mrushidany <mrushidany@gmail.com>
2022-02-28 13:46:04 +0300

    Removed the soft delete for now

--- app/Models/HardwareCategory.php
+++ app/Models/HardwareCategory.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class HardwareCategory extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     public function hardware_sub_categories()
     {
--- app/Models/HardwareSubCategory.php
+++ app/Models/HardwareSubCategory.php
@@ -4,11 +4,10 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class HardwareSubCategory extends Model
 {
-    use HasFactory, SoftDeletes;
+    use HasFactory;
 
     public function hardware_main_category()
     {
