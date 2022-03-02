34d3e9223c38c901e3082dd30b0a21524747b1ca
mrushidany <mrushidany@gmail.com>
2022-03-02 09:17:20 +0300

    Managed to pass the name of the modal

--- app/Http/Controllers/HardwareCategoriesController.php
+++ app/Http/Controllers/HardwareCategoriesController.php
@@ -122,8 +122,9 @@ class HardwareCategoriesController extends Controller
                             ->get();
         return DataTables::of($category_list)
                             ->addColumn('action', function ($list) {
+                                $modal = 'hardware_sub_category_modal';
                                 $button = '';
-                                $button .= '<a href="javascript:edit(\''. route('hardware_categories.update', $list->id) .'\')" class="btn btn-sm p-0 " data-bs-toggle="tooltip" data-bs-placement="top" title="Edit" data-bs-original-title="Edit" aria-label="Edit"><span class="text-500 fas fa-edit"></span></a>';
+                                $button .= '<a href="javascript:edit(\''. route('hardware_categories.update', $list->id) .'\', \''. $modal .'\')" class="btn btn-sm p-0 " data-bs-toggle="tooltip" data-bs-placement="top" title="Edit" data-bs-original-title="Edit" aria-label="Edit"><span class="text-500 fas fa-edit"></span></a>';
                                 $button .= '<a href="javascript:destroy(\''. route('hardware_categories.destroy',$list->id) .'\')" class="btn btn-sm p-0 ms-2" data-bs-toggle="tooltip" data-bs-placement="top" title="Delete" data-bs-original-title="Delete" aria-label="Delete" aria-describedby="tooltip253699"><span class="text-500 fas fa-trash-alt"></span></a>';
                                 return '<nobr>'. $button . '</nobr>';
                             })
