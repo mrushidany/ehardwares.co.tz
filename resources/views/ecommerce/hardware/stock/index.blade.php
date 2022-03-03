65976142591ec86866dddcfbcfd59b8c283ab474
mrushidany <mrushidany@gmail.com>
2022-03-03 12:07:28 +0300

    Hardware stock section breadcrumb

--- resources/views/ecommerce/hardware/stock/index.blade.php
+++ resources/views/ecommerce/hardware/stock/index.blade.php
@@ -1,7 +1,20 @@
 @extends('ecommerce.admin.main')
 
 @section('content')
-
+<div class="row g-0">
+    <div class="card mb-3">
+        <div class="card-header">
+            <div class="row flex-between-center">
+                <div class="col">
+                    <nav style="--falcon-breadcrumb-divider: '»';" aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                          <li class="breadcrumb-item"><a href="{{ route('dashboard') }}">Dashboard</a></li>
+                          <li class="breadcrumb-item active" aria-current="page">Hardware Stock</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
 
 
 
