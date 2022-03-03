22135128a18d2669c32f07fa01ebf96f1219ddc4
mrushidany <mrushidany@gmail.com>
2022-03-03 16:18:35 +0300

    Added a select option in the stock form

---
+++ resources/views/ecommerce/hardware/stock/forms/stock_form.blade.php
@@ -0,0 +1,36 @@
+<div class="modal-dialog modal-dialog-centered" role="document" style="max-width: 900px">
+    <div class="modal-content position-relative">
+        <div class="position-absolute top-0 end-0 mt-2 me-2 z-index-1">
+            <button class="btn-close btn btn-sm btn-circle d-flex flex-center transition-base close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <form class="hardware_category_form">
+            <div class="modal-body p-0">
+                <div class="rounded-top-lg py-3 ps-4 pe-6 bg-light text-center">
+                    <h4 class="mb-1" id="modalExampleDemoLabel">Add a new hardware stock </h4>
+                </div>
+                <div class="p-4 pb-0">
+                    <div class="row">
+                        <div class="mb-3 col-4">
+                            <label class="col-form-label" for="name">Name</label>
+                            <input class="form-control" name="name" type="text" value="">
+                        </div>
+                        <div class="mb-3 col-4">
+                            <label class="col-form-label" for="category">Category</label>
+                            <select class="form-select js-choice main_category" id="organizerSingle" size="1" name="main_category_id" data-options='{"removeItemButton":true,"placeholder":true}'>
+                                <option value="">Select main category</option>
+                                @foreach ($main_categories as $category)
+                                    <option value="{{ $category->id }}">{{ $category->category_name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button class="btn btn-outline-warning me-1 mb-1 reset_hardware_category" type="reset">Reset</button>
+                <button class="btn btn-outline-success me-1 mb-1 save_hardware_category" type="button">Save</button>
+                <button class="btn btn-outline-success me-1 mb-1 disabled loading_button" style="display: none;" type="button">Saving .....</button>
+            </div>
+        </form>
+    </div>
+</div>
