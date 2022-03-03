f7ac6902602c0c27b88a665bc08e59b2fafb896e
mrushidany <mrushidany@gmail.com>
2022-03-03 11:56:27 +0300

    hardware stock details table migration table

--- database/migrations/2022_03_03_112913_create_hardware_stock_details_table.php
+++ database/migrations/2022_03_03_112913_create_hardware_stock_details_table.php
@@ -15,6 +15,14 @@ class CreateHardwareStockDetailsTable extends Migration
     {
         Schema::create('hardware_stock_details', function (Blueprint $table) {
             $table->id();
+            $table->string('units');
+            $table->integer('quantity');
+            $table->string('raw_price');
+            $table->string('selling_price');
+            $table->unsignedBigInteger('hardware_stock_id');
+            $table->string('image_url');
+            $table->foreign('hardware_stock_id')->references('id')->on('hardware_stocks')->cascadeOnDelete()->cascadeOnUpdate();
+            $table->softDeletesTz($column = 'deleted_at', $precision = 0);
             $table->timestamps();
         });
     }
