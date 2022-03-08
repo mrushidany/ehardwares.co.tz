91783e7f68cbc2af69d69b4e53dcf51a9dd9c79d
Dany <mrushidany@gmail.com>
2022-03-08 15:28:44 +0300

    installed laravel mix

--- public/js/app.js
+++ public/js/app.js
@@ -5448,8 +5448,6 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var alpinejs__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! alpinejs */ "./node_modules/alpinejs/dist/module.esm.js");
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
-__webpack_require__(/*! ./components/LandingPage.js */ "./resources/js/components/LandingPage.js");
-
 
 window.Alpine = alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"];
 alpinejs__WEBPACK_IMPORTED_MODULE_0__["default"].start();
@@ -5484,16 +5482,6 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
-
-/***/ }),
-
-/***/ "./resources/js/components/LandingPage.js":
-/*!************************************************!*\
-  !*** ./resources/js/components/LandingPage.js ***!
-  \************************************************/
-/***/ (() => {
-
-throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nSyntaxError: C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\resources\\js\\components\\LandingPage.js: Support for the experimental syntax 'jsx' isn't currently enabled (7:9):\n\n\u001b[0m \u001b[90m  5 |\u001b[39m \u001b[36mexport\u001b[39m \u001b[36mdefault\u001b[39m \u001b[36mfunction\u001b[39m \u001b[33mLandingPage\u001b[39m(){\u001b[0m\n\u001b[0m \u001b[90m  6 |\u001b[39m     \u001b[36mreturn\u001b[39m (\u001b[0m\n\u001b[0m\u001b[31m\u001b[1m>\u001b[22m\u001b[39m\u001b[90m  7 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m    |\u001b[39m         \u001b[31m\u001b[1m^\u001b[22m\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m  8 |\u001b[39m             \u001b[33m<\u001b[39m\u001b[33mp\u001b[39m\u001b[33m>\u001b[39m\u001b[33mHello\u001b[39m \u001b[33mEhardwares\u001b[39m\u001b[33m.\u001b[39mco\u001b[33m.\u001b[39mtz\u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mp\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m  9 |\u001b[39m         \u001b[33m<\u001b[39m\u001b[33m/\u001b[39m\u001b[33mdiv\u001b[39m\u001b[33m>\u001b[39m\u001b[0m\n\u001b[0m \u001b[90m 10 |\u001b[39m     )\u001b[33m;\u001b[39m\u001b[0m\n\nAdd @babel/preset-react (https://git.io/JfeDR) to the 'presets' section of your Babel config to enable transformation.\nIf you want to leave it as-is, add @babel/plugin-syntax-jsx (https://git.io/vb4yA) to the 'plugins' section to enable parsing.\n    at Parser._raise (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:569:17)\n    at Parser.raiseWithData (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:562:17)\n    at Parser.expectOnePlugin (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:3626:18)\n    at Parser.parseExprAtom (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:12055:18)\n    at Parser.parseExprSubscripts (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:11654:23)\n    at Parser.parseUpdate (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:11634:21)\n    at Parser.parseMaybeUnary (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:11609:23)\n    at Parser.parseMaybeUnaryOrPrivate (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:11421:61)\n    at Parser.parseExprOps (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:11428:23)\n    at Parser.parseMaybeConditional (C:\\xampp\\htdocs\\Ehardwares\\ehardwares.co.tz\\node_modules\\@babel\\parser\\lib\\index.js:11398:23)");
 
 /***/ }),
 
