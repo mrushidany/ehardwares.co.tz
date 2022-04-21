40b5d51892851f9751843c140bd34c234cc4a89d
Dany <mrushidany@gmail.com>
2022-04-21 15:50:05 +0300

    ehardwares icon

---
+++ resources/views/ecommerce/website/index.blade.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <meta name="theme-color" content="">
+    <link rel="preconnect" href="https://cdn.shopify.com/" crossorigin>
+    <link rel="preconnect" href="https://fonts.shopify.com/" crossorigin>
+    <link rel="preconnect" href="https://monorail-edge.shopifysvc.com/">
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('ecommerce/img/icon.jpeg') }}">
+    <title>ehardwares</title>
+</head>
+<body>
+
+</body>
+</html>
