9329a6186e6082f589164954aae54b2832a26673
Dany <mrushidany@gmail.com>
2022-04-27 17:25:37 +0300

    Styling the horizontal line

--- resources/views/ecommerce/website/index.blade.php
+++ resources/views/ecommerce/website/index.blade.php
@@ -21,7 +21,13 @@
                     <a href="#" class="font-medium py-2  rounded-sm hover:bg-slate-100 hover:text-slate-900 about">About Us</a>
                 </ul>
             </nav>
-            <hr class="hr mx-auto my-4"></hr>
+            <div>
+                <hr class="hr mx-auto"></hr>
+            </div>
+
+            <div>
+                <p>kjkjk</p>
+            </div>
         </div>
 
 
