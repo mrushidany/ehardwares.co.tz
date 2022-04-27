313208d0d356fbbeb10ec1ef6939ae8feb3c62ff
Dany <mrushidany@gmail.com>
2022-04-27 16:26:30 +0300

    Navigation link

--- resources/views/ecommerce/website/index.blade.php
+++ resources/views/ecommerce/website/index.blade.php
@@ -9,16 +9,19 @@
     <title>ehardwares</title>
 </head>
 <body>
-    <div class="header">
-        <div class="nav">
-            <nav class="flex justify-start space-x-4">
-                <a href="/dashboard" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
-  <a href="/team" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Team</a>
-  <a href="/projects" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Projects</a>
-  <a href="/reports" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Reports</a>
-            </nav>
+    <div class="app w-16 md:w-32 lg:w-48">
+        <div class="header">
+            <div class="nav">
+                <nav class="flex justify-start space-x-4">
+                    <a href="#" class="font-medium px-3 py-2 text-slate-700 rounded-sm hover:bg-slate-100 hover:text-slate-900 chat">Chat with us</a>
+                    <a href="#" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Team</a>
+                    <a href="#" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Projects</a>
+                    <a href="#" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Reports</a>
+                </nav>
+            </div>
         </div>
     </div>
+
 
 </body>
 </html>
