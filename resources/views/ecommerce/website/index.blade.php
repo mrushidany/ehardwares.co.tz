a9b1f72746f4298db6642c08525fc069c728c588
Dany <mrushidany@gmail.com>
2022-04-27 12:58:14 +0300

    UNinstall tailwind

--- resources/views/ecommerce/website/index.blade.php
+++ resources/views/ecommerce/website/index.blade.php
@@ -3,11 +3,15 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('ecommerce/img/icon.jpeg') }}"> 
-    <link rel="stylesheet" href="{{ asset('ecommerce/assets/web/main.css')}}"> 
+    <link rel="shortcut icon" type="image/x-icon" href="{{ asset('ecommerce/img/icon.jpeg') }}">
+    <link rel="stylesheet" href="{{ asset('ecommerce/assets/web/main.css')}}">
+    <link  rel="{{ asset('css/app.css')}}"/>
     <title>ehardwares</title>
 </head>
 <body>
+    <p class="text-3xl font-bold underline">
+        Hello world!
+      </p>
 
 </body>
 </html>
