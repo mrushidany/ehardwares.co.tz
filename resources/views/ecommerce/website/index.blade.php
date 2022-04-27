2b4874cf120d3c7f15fed0ad8bccbb0e151d8a06
Dany <mrushidany@gmail.com>
2022-04-27 16:12:22 +0300

    Style the navigation

--- resources/views/ecommerce/website/index.blade.php
+++ resources/views/ecommerce/website/index.blade.php
@@ -9,7 +9,16 @@
     <title>ehardwares</title>
 </head>
 <body>
-
+    <div class="header">
+        <div class="nav">
+            <nav class="flex justify-start space-x-4">
+                <a href="/dashboard" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Home</a>
+  <a href="/team" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Team</a>
+  <a href="/projects" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Projects</a>
+  <a href="/reports" class="font-medium px-3 py-2 text-slate-700 rounded-lg hover:bg-slate-100 hover:text-slate-900">Reports</a>
+            </nav>
+        </div>
+    </div>
 
 </body>
 </html>
