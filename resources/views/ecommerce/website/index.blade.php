8cfe89ded34d1de1e0203d4d4b863f91c3cd805b
Dany <mrushidany@gmail.com>
2022-05-02 16:27:03 +0300

    Removed the header message start

--- resources/views/ecommerce/website/index.blade.php
+++ resources/views/ecommerce/website/index.blade.php
@@ -60,49 +60,6 @@
     <!-- Header Area start  -->
     <div class="header section">
         <!-- Header Top Message Start -->
-        <div class="header-top section-fluid bg-black">
-            <div class="container">
-                <div class="row row-cols-lg-2 align-items-center">
-                    <!-- Header Top Message Start -->
-                    <div class="col text-center text-lg-start">
-                        <div class="header-top-massege">
-                            <p>Default Welcome Message </p>
-                        </div>
-                    </div>
-                    <!-- Header Top Message End -->
-                    <!-- Header Top Language Currency -->
-                    <div class="col d-none d-lg-block">
-                        <div class="header-top-set-lan-curr d-flex justify-content-end">
-                            <!-- Single Wedge Start -->
-                            <div class="header-top-curr dropdown">
-                                <button class="dropdown-toggle" data-bs-toggle="dropdown"> Language : (ENG) <i
-                                        class="ion-ios-arrow-down"></i></button>
-                                <ul class="dropdown-menu dropdown-menu-right">
-                                    <li><a class="dropdown-item" href="#">English</a></li>
-                                    <li><a class="dropdown-item" href="#">Italiano</a></li>
-                                    <li><a class="dropdown-item" href="#">Français</a></li>
-                                    <li><a class="dropdown-item" href="#">Filipino</a></li>
-                                </ul>
-                            </div>
-                            <!-- Single Wedge End -->
-                            <!-- Single Wedge Start -->
-                            <div class="header-top-curr dropdown">
-                                <button class="dropdown-toggle pr-0 border-0" data-bs-toggle="dropdown">Currency : (USD)
-                                    <i class="ion-ios-arrow-down"></i></button>
-                                <ul class="dropdown-menu dropdown-menu-right">
-                                    <li><a class="dropdown-item" href="#">USD $</a></li>
-                                    <li><a class="dropdown-item" href="#">EUR €</a></li>
-                                    <li><a class="dropdown-item" href="#">POUND £</a></li>
-                                    <li><a class="dropdown-item" href="#">FRANC ₣</a></li>
-                                </ul>
-                            </div>
-                            <!-- Single Wedge End -->
-                        </div>
-                    </div>
-                    <!-- Header Top Language Currency -->
-                </div>
-            </div>
-        </div>
         <!-- Header Top  End -->
         <!-- Header Bottom  Start -->
         <div class="header-bottom d-none d-lg-block">
