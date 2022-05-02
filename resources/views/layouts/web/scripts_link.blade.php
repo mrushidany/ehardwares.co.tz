c7cb0b8cf7454767c9ec79799b0ba3782f5230c7
Dany <mrushidany@gmail.com>
2022-05-02 18:18:31 +0300

    Include the footer scripts link

---
+++ resources/views/layouts/web/scripts_link.blade.php
@@ -0,0 +1,7 @@
+<!-- Use the minified version files listed below for better performance and remove the files listed above -->
+<script src="{{ asset ('ecommerce/assets/web/dist/js/vendor/vendor.min.js') }}"></script>
+<script src="{{ asset ('ecommerce/assets/web/dist/js/plugins/plugins.min.js') }}"></script>
+
+<!-- Main Js -->
+<script src="{{ asset ('ecommerce/assets/web/dist/js/main.js') }}"></script>
+</body>
