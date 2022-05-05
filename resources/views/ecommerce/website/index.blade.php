55f36217e00aa8c2a1f27f480b28697de72da94d
Dany <mrushidany@gmail.com>
2022-05-05 10:33:27 +0300

    Included the footer file

--- resources/views/ecommerce/website/index.blade.php
+++ resources/views/ecommerce/website/index.blade.php
@@ -1433,25 +1433,7 @@
     <!-- Banner Section End -->
 
     <!-- Footer Area Start -->
-    <div class="footer-area">
-        <div class="footer-container">
-            <div class="footer-bottom">
-                <div class="container">
-                    <div class="row flex-sm-row-reverse">
-                        <div class="col-md-6 text-end">
-                            <div class="payment-link">
-                                <img src="assets/images/icons/payment.png" alt="">
-                            </div>
-                        </div>
-                        <div class="col-md-6 text-start">
-                            <p class="copy-text"> &copy;&nbsp;<?php echo date('Y') ?> <strong>ehardwares</strong> Made By <a class="company-name" href="https://valartech.co.tz">
-                                <strong> Valar Technology Solutions</strong></a>.</p>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('layouts.web.footer')
     <!-- Footer Area End -->
 
 
---
+++ resources/views/layouts/web/footer.blade.php
@@ -0,0 +1,19 @@
+<div class="footer-area">
+    <div class="footer-container">
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="row flex-sm-row-reverse">
+                    <div class="col-md-6 text-end">
+                        <div class="payment-link">
+                            <img src="assets/images/icons/payment.png" alt="">
+                        </div>
+                    </div>
+                    <div class="col-md-6 text-start">
+                        <p class="copy-text"> &copy;&nbsp;<?php echo date('Y') ?> <strong>ehardwares</strong> | <a class="company-name" href="https://valartech.co.tz">
+                            <strong> Valar Technology Solutions</strong></a>.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
