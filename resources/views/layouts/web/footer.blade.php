2a1e1ab246ff9008ed3442a63ad35252bc13cd60
Dany <mrushidany@gmail.com>
2022-05-19 11:08:17 +0300

    Injected the google map

--- resources/views/layouts/web/footer.blade.php
+++ resources/views/layouts/web/footer.blade.php
@@ -1,5 +1,20 @@
 <div class="footer-area">
     <div class="footer-container">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row">
+                    <!-- Start single blog -->
+                    {{-- <div class="col-md-12 col-lg-12 " data-aos="fade-up" data-aos-delay="200"> --}}
+
+                            <iframe
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3961.950984313895!2d39.20678021427425!3d-6.775820668144869!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x185c4eef779ead53%3A0x9caf60e5992e2586!2sKariakoo!5e0!3m2!1sen!2stz!4v1651921428515!5m2!1sen!2stz"
+                                width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                            </iframe>
+
+                    {{-- </div> --}}
+                </div>
+            </div>
+        </div>
         <div class="footer-bottom">
             <div class="container">
                 <div class="row flex-sm-row-reverse">
