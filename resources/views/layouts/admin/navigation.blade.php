34d3c68fb5c36c28554da53e17769de8616a5f4b
Dany <mrushidany@gmail.com>
2022-05-20 12:08:57 +0300

    Added the ehardwares link to the admin dashboard

--- resources/views/layouts/admin/navigation.blade.php
+++ resources/views/layouts/admin/navigation.blade.php
@@ -29,6 +29,25 @@
                     </a>
                 </li>
 
+                {{-- Ecommerce site link --}}
+                <li class="nav-item">
+                    <!--Ecommerce Site label-->
+                    <div class="row navbar-vertical-label-wrapper mt-3 mb-2">
+                        <div class="col-auto navbar-vertical-label">Ecommerce Site</div>
+                        <div class="col ps-0">
+                            <hr class="mb-0 navbar-vertical-divider" />
+                        </div>
+                    </div>
+                    <a class="nav-link" href="{{ route('landing_page') }}" role="button" aria-expanded="false">
+                        <div class="d-flex align-items-center">
+                            <span class="nav-link-icon">
+                                <span class="fas fa-globe"></span>
+                            </span>
+                            <span class="nav-link-text ps-1">ehardwares.co.tz</span>
+                        </div>
+                      </a>
+                </li>
+
                 {{-- Operations Navigational Links --}}
                 <li class="nav-item">
                     <!--OPerations label-->
