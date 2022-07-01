49272e9cbb92d3cb45f1f1cb279b403e117e733d
Dany <mrushidany@gmail.com>
2022-07-01 11:27:25 +0300

    Re align the database settings accordion

--- resources/views/ecommerce/admin/super/settings/app/index.blade.php
+++ resources/views/ecommerce/admin/super/settings/app/index.blade.php
@@ -22,6 +22,24 @@
                     <div class="accordion" id="accordionExample">
                         <div class="accordion-item">
                             <h2 class="accordion-header" id="heading1">
+                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#configuration_settings" aria-expanded="true" aria-controls="collapse2">
+                                    <span class="fas fa-rocket me-1" data-fa-transform="shrink-3"></span> Configuration Settings
+                                </button>
+                            </h2>
+                            <div class="accordion-collapse collapse" id="configuration_settings" aria-labelledby="heading2" data-bs-parent="#accordionExample">
+                                <div class="accordion-body">
+                                    <div class="row">
+                                        <div class="col-md-4">
+                                            <button class="btn btn-falcon-danger me-1 mb-1 clear_cache" type="button">
+                                                <span class="far fa-trash-alt me-1" data-fa-transform="shrink-3"></span> Clear Cache
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="accordion-item">
+                            <h2 class="accordion-header" id="heading2">
                                 <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#database_settings" aria-expanded="true" aria-controls="collapse1">
                                     <span class="fas fa-database me-1" data-fa-transform="shrink-3"></span> Database Settings
                                 </button>
@@ -37,24 +55,6 @@
                                         <div class="col-md-4">
                                             <button class="btn btn-falcon-default me-1 mb-1 dg_migrate_rollback" type="button">
                                                 <span class="fas fa-undo me-1" data-fa-transform="shrink-3"></span> DB Migrate RollBack
-                                            </button>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="accordion-item">
-                            <h2 class="accordion-header" id="heading2">
-                                <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#configuration_settings" aria-expanded="true" aria-controls="collapse2">
-                                    <span class="fas fa-rocket me-1" data-fa-transform="shrink-3"></span> Configuration Settings
-                                </button>
-                            </h2>
-                            <div class="accordion-collapse collapse" id="configuration_settings" aria-labelledby="heading2" data-bs-parent="#accordionExample">
-                                <div class="accordion-body">
-                                    <div class="row">
-                                        <div class="col-md-4">
-                                            <button class="btn btn-falcon-danger me-1 mb-1 clear_cache" type="button">
-                                                <span class="far fa-trash-alt me-1" data-fa-transform="shrink-3"></span> Clear Cache
                                             </button>
                                         </div>
                                     </div>
